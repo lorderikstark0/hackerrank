e6ee49b8eaaa02e3952a32aa4fc7ee52f27d3511
lorderikstark0 <sagnik.chatterjee61@gmail.com>
2020-06-14 18:21:55 +0530

    added java files

---
+++ java/welcomeJava.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class welcomeJava{
+    public static void main(String[] main){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
